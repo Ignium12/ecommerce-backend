1d878d989c1cffef9560c33a2f51b421af08950e
Ignium12 <kevin.ruehl12@googlemail.com>
2021-02-16 19:52:54 +0100

    Rest GET methods now also deliver the id of the entities in the JSON file

--- src/main/java/com/kruehl/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/kruehl/ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,30 @@ package com.kruehl.ecommerce.config;
 
 import com.kruehl.ecommerce.entity.Product;
 import com.kruehl.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired // optional
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
 
     // configure which methods are allowed
     @Override
@@ -26,5 +42,22 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+
+        // call an interal helper method
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for(EntityType temp: entities){
+            entityClasses.add(temp.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+
+        config.exposeIdsFor(domainTypes);
     }
 }
